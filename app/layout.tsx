1b4658fd937e439acb932f2a91e27c0249ed48c4
louhanquo <alfalionellofann@gmail.com>
2024-05-29 01:13:57 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <!-- Google tag (gtag.js) -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=G-DLGMVPS7FL"></script>
+          <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          
+          gtag('config', 'G-DLGMVPS7FL');
+          </script>
+      </body>
     </html>
   );
 }
